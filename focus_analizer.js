cc32d645f53e10fafc31a78711f3e49b18621f67
2bdlb-rus <wildhunterbyisaac@mail.ru>
2025-10-24 23:54:17 +0400

    исправление стурктуры кода, и удаление лишних деталей

--- focus_analizer.js
+++ focus_analizer.js
@@ -1,6 +1,5 @@
-const days = ["Mon","Tue","Wed","Thu","Fri"];for (const d of days);
-const readline = require('readline');
 
+const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -11,21 +10,21 @@ async function askQuestion(question) {
   });
 }
 async function main() {
-    const Montime = await askQuestion('Cколько времени ты фокусировался в Понедельник? ');
-    const Tuetime = await askQuestion("Сколько времени ты фокусировался во Вторник? ");
-    const WedTime = await askQuestion("Сколько времени ты фокусировался в Среду? ");
-    const ThuTime = await askQuestion("Сколько времени ты фокусировался в Четверг? ");
-    const FriTime = await askQuestion("Сколько времени ты фокусировался в Пятницу? ");
-let Average = (parseInt(Montime) + parseInt(Tuetime) + parseInt(WedTime) + parseInt(ThuTime) + parseInt(FriTime)) /5;
-const day = new Map();
-day.set(mon,parseInt(Montime));
-day.set(tue,parseInt(Tuetime));
-day.set(wed,parseInt(WedTime));
-day.set(thu,parseInt(ThuTime));
-day.set(fri,parseInt(FriTime));
+  const Montime = await askQuestion('Cколько времени ты фокусировался в Понедельник? ');
+  const Tuetime = await askQuestion("Сколько времени ты фокусировался во Вторник? ");
+  const WedTime = await askQuestion("Сколько времени ты фокусировался в Среду? ");
+  const ThuTime = await askQuestion("Сколько времени ты фокусировался в Четверг? ");
+  const FriTime = await askQuestion("Сколько времени ты фокусировался в Пятницу? ");
+  let Average = (parseInt(Montime) + parseInt(Tuetime) + parseInt(WedTime) + parseInt(ThuTime) + parseInt(FriTime)) / 5;
+  const day = new Map();
+  day.set('Monday', parseInt(Montime));
+  day.set('Tuesday', parseInt(Tuetime));
+  day.set('Wednesday', parseInt(WedTime));
+  day.set('Thursday', parseInt(ThuTime));
+  day.set('Friday', parseInt(FriTime));
 
 
-console.log("Average focus: "+ Average+ " hours/day");
+  console.log("Average focus: " + Average + " hours/day");
 
 }
 
